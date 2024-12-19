920a10c7218494c86fd47df3429d412659ec06bb
bee-xin <bijen.ito@gmail.com>
2024-12-19 14:46:16 +0545

    arraymethods

---
+++ react/arraypt2.js
@@ -0,0 +1,4 @@
+const colors=["Red", "Blue", "White", "Green"];
+document.getElementById("work").innerHTML=colors;
+colors.sort();
+document.getElementById("smart").innerHTML=colors;
---
+++ react/arraysort.js
@@ -0,0 +1,4 @@
+const names=["Bijen", "Aashish","Mohan", "Milan"];
+document.getElementById("one").innerHTML=names;
+names.sort();
+document.getElementById("two").innerHTML=names;
